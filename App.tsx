65ad0d914a3d3b9c754b56742e80e94f225d3c30
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-04 20:57:16 -0300

    feat: app load google fonts

--- App.tsx
+++ App.tsx
@@ -3,8 +3,15 @@ import theme from "@theme/index";
 import React from "react";
 import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components/native";
+import {
+  Nunito_400Regular,
+  Nunito_700Bold,
+  useFonts,
+} from "@expo-google-fonts/nunito";
+import { Loading } from "@components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Nunito_400Regular, Nunito_700Bold });
   return (
     <ThemeProvider theme={theme}>
       <StatusBar
@@ -12,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <Home />
+      {fontsLoaded ? <Home /> : <Loading />}
     </ThemeProvider>
   );
 }
