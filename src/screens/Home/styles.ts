4928c44f03a95c7753f4f5af622596505937dcca
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-04 20:59:30 -0300

    feat: home styles

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,8 +3,16 @@ import styled from "styled-components/native";
 export const Container = styled.View`
   background-color: ${({ theme }) => theme.COLORS.GRAY_700};
   flex: 1;
+  margin-top: 40px;
+  padding: 0 24px;
 `;
 
 export const Text = styled.Text`
-  font-size: 50px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  margin-bottom: 8px;
+`;
+
+export const AreaAddMeals = styled.View`
+  padding: 45px 0;
 `;
