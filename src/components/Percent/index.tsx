fc4b56391bfc9185dbf47cb24e7f5fb3c0831bc2
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-07 21:10:02 -0300

    feat: add percent props

--- src/components/Percent/index.tsx
+++ src/components/Percent/index.tsx
@@ -7,6 +7,7 @@ import {
   AreaPercentage,
 } from "./styles";
 import { ButtonIcon } from "@components/ButtonIcon";
+import { useNavigation } from "@react-navigation/native";
 
 type Props = {
   positionArrow: "flex-start" | "flex-end";
@@ -14,6 +15,16 @@ type Props = {
 };
 
 export function Percent({ positionArrow, percentageValue }: Props) {
+  const { navigate } = useNavigation();
+
+  function handleSubmit() {
+    if (positionArrow === "flex-end") {
+      navigate("statistic", { percent: percentageValue });
+    } else {
+      navigate("home");
+    }
+  }
+
   return (
     <Container isFitness={percentageValue > 50 ? true : false}>
       <AreaArrow
@@ -21,6 +32,7 @@ export function Percent({ positionArrow, percentageValue }: Props) {
       >
         <ButtonIcon
           icon={positionArrow === "flex-end" ? "arrow-up-right" : "arrow-left"}
+          onPress={handleSubmit}
         />
       </AreaArrow>
       <AreaPercentage>
